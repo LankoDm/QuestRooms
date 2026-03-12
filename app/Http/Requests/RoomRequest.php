14bda7b524761fdcbb11b85a716d72a6c5837a8e
LankoDm <play585588@gmail.com>
2026-03-12 17:34:51 +0200

    Added API controller and validation for quest rooms

---
+++ app/Http/Requests/RoomRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:10|max:255|unique:rooms,name',
+            'description' => 'required|min:20',
+            'difficulty' => 'required|in:easy,medium,hard,ultra hard',
+            'image_path' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'min_players' => 'required|integer|min:1',
+            'max_players' => 'required|integer|gte:min_players',
+            'weekday_price' => 'required|integer|min:0',
+            'weekend_price' => 'required|integer|min:0',
+            'duration_minutes' => 'required|integer|min:10',
+            'is_active' => 'required|boolean',
+            'slug' => 'required|unique:rooms,slug',
+        ];
+    }
+
+//    public function messages(): array //Дописать потом
+//    {
+//        return [
+//
+//        ];
+//    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('/rooms', RoomController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
